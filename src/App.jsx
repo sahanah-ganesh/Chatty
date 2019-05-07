8b3991d9588591ecb94280b02be45cb50e03a18c
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-05-07 14:16:57 +0000

    passed props to render username and messages

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,32 @@ import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: { name: 'Bob' }, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          id: 1,
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?',
+        },
+        {
+          id: 2,
+          username: 'Anonymous',
+          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <MessageList />
-        <ChatBar />
+        <MessageList messages={this.state.messages}/>
+        <ChatBar currentUser={this.state.currentUser}/>
       </div>
     );
   }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 import Message from './Message.jsx';
 
-{/*needs function*/}
-
 class MessageList extends Component {
   render() {
+    let messageContent = this.props.messages.map((message) =>
+      <Message key={message.id} username={message.username} content={message.content} />
+    );
     return (
       <main className="messages">
-        <Message />
+        {messageContent}
       </main>
     );
   }
