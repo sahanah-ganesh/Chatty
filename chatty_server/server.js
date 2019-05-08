97f462d6352ed433faa03e314eccd58686093bb1
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-05-08 01:37:39 +0000

    midway to connecting clients

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
+const uuidV1 = require('uuid/v1');
 
 // Set the port to 3001
 const PORT = 3001;
@@ -13,12 +14,25 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+//Broadcast to all
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket)
+  }
+}
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
+  ws.on('message', (msg) => {
+    const receiveMsg = JSON.parse(msg);
+    console.log('received', receiveMsg);
+
+
+  }
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
+
 });
